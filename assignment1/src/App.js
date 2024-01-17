12665c183e40766ba9367222ec2f3482a0138596
GurkiratSinghArora <sgurkirat868@gmail.com>
2024-01-17 11:20:55 -0800

    Created react app for Assignment1

---
+++ assignment1/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='mainContainer'>
+      <div className='navBar'>Recipe App</div>
+
+    </div>
+  );
+}
+
+export default App;
