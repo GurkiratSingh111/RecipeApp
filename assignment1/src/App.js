51c75d62d12baf0e144a4e0e136666098c06ccd6
GurkiratSinghArora <sgurkirat868@gmail.com>
2024-01-17 12:07:45 -0800

    Added navigation bar

--- assignment1/src/App.js
+++ assignment1/src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className='mainContainer'>
-      <div className='navBar'>Recipe App</div>
+      <div className="bg-slate-800 h-16">
+        <div className='py-4 text-white px-10 text-2xl'>Recipe App</div>
+
+      </div>
 
     </div>
   );
