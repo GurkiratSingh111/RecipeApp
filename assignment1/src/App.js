d03db43d79d4ee84ce154b9d3eb1b7f0fec832fe
GurkiratSinghArora <sgurkirat868@gmail.com>
2024-01-17 18:34:08 -0800

    Implemented AddRecipe component

--- assignment1/src/App.js
+++ assignment1/src/App.js
@@ -1,12 +1,10 @@
 import { useState } from "react";
-import Header from "./components/Header";
-import Input from "./components/Input";
 import Navbar from "./components/Navbar";
-import Button from "./components/Button";
+import AddRecipe from "./components/AddRecipe";
 
 function App() {
   const [recipe, setRecipe] = useState({});
-  const [allRecipe, setAllRecipe] = useState([])
+  const [allRecipe, setAllRecipe] = useState([]);
   return (
     <div className="h-full w-full text-white">
       <Navbar />
@@ -19,22 +17,7 @@ function App() {
           backgroundSize: "cover",
         }}
       >
-        <div className="my-8 pb-5 flex flex-col items-center border-solid border-4 border-yellow-950 rounded-3xl w-4/5 h-full ml-10 mr-10 ">
-          <Header mainHeading={true}>Add New Recipe</Header>
-          <div className="flex flex-col w-4/5 items-center">
-            <Header>Recipe Name</Header>
-            <Input />
-            <Header>Ingredients</Header>
-            <Input textarea={true} />
-            <Header>Directions</Header>
-            <Input textarea={true} />
-            <div className="flex flex-row justify-center items-center flex-1">
-            <Button>Reset</Button>
-            <Button>Save</Button>
-            </div>
-            
-          </div>
-        </div>
+        <AddRecipe/>
       </div>
     </div>
   );
