c4c72a252f92711fbd0c704c85aff3f2bb752978
GurkiratSinghArora <sgurkirat868@gmail.com>
2024-01-18 20:29:57 -0800

    Completed the basic All Recipe component

--- assignment1/src/components/AllRecipe.js
+++ assignment1/src/components/AllRecipe.js
@@ -1,8 +1,19 @@
-function AllRecipe(){
-    return <div>
-        Hello
-    </div>
+import Header from "./Header";
 
+function AllRecipe({allRecipe}){
+    console.log(allRecipe);
+    return <div className="my-8 pb-5 flex flex-col items-center border-solid border-4 border-yellow-950 rounded-3xl w-4/5 h-full ml-10 mr-10 ">
+        <Header mainHeading={true}>Find your Saved Recipes Here</Header>
+        <div className=" bg-yellow-950 my-8 pb-5 flex flex-col items-center border-solid border-4 border-yellow-950 rounded-3xl w-4/5 h-full ml-10 mr-10 ">
+            {allRecipe.map((recipe)=>{
+                return (
+                <div className="flex flex-col w-4/5 items-left">
+                    <li className="text-3xl font-bold ">{recipe.name}</li>
+                </div>)
+            })
+}</div>
+</div>
 }
+
 
 export default AllRecipe;
--- assignment1/src/components/Header.js
+++ assignment1/src/components/Header.js
@@ -2,7 +2,7 @@ function Header ({children, mainHeading}){
 
     if (mainHeading){
         return (<div className='bg-yellow-950 border-solid border-4 h-10 border-yellow-950 rounded-3xl w-4/5 flex items-center my-2 justify-center' >
-        <div className='h-6 text-xl'>Add New Recipe </div>
+        <div className='h-6 text-xl'>{children}</div>
       </div>)
     }
 
