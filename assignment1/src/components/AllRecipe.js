38c4a500deb04f6393bcfafcd61546b68de182df
GurkiratSinghArora <sgurkirat868@gmail.com>
2024-01-18 20:54:21 -0800

    Changed the text sizes for the Header component

--- assignment1/src/components/AllRecipe.js
+++ assignment1/src/components/AllRecipe.js
@@ -4,11 +4,11 @@ function AllRecipe({allRecipe}){
     console.log(allRecipe);
     return <div className="my-8 pb-5 flex flex-col items-center border-solid border-4 border-yellow-950 rounded-3xl w-4/5 h-full ml-10 mr-10 ">
         <Header mainHeading={true}>Find your Saved Recipes Here</Header>
-        <div className=" bg-yellow-950 my-8 pb-5 flex flex-col items-center border-solid border-4 border-yellow-950 rounded-3xl w-4/5 h-full ml-10 mr-10 ">
+        <div className=" bg-yellow-950 my-8 py-5 flex flex-col items-center border-solid border-4 border-yellow-950 rounded-3xl w-4/5 h-full ml-10 mr-10 ">
             {allRecipe.map((recipe)=>{
                 return (
                 <div className="flex flex-col w-4/5 items-left">
-                    <li className="text-3xl font-bold ">{recipe.name}</li>
+                    <li className="text-lg">{recipe.name}</li>
                 </div>)
             })
 }</div>
--- assignment1/src/components/Header.js
+++ assignment1/src/components/Header.js
@@ -1,8 +1,8 @@
 function Header ({children, mainHeading}){
 
     if (mainHeading){
-        return (<div className='bg-yellow-950 border-solid border-4 h-10 border-yellow-950 rounded-3xl w-4/5 flex items-center my-2 justify-center' >
-        <div className='h-6 text-xl'>{children}</div>
+        return (<div className='bg-yellow-950 border-solid border-4 h-10 border-yellow-950 rounded-3xl w-4/5 flex items-center my-4 justify-center' >
+        <div className='h-6 text-2xl my-8'>{children}</div>
       </div>)
     }
 
