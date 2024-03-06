668cd13ca17f24cddb37f70b6e4fb33ccda9f4e4
GurkiratSinghArora <sgurkirat868@gmail.com>
2024-03-05 21:37:08 -0800

    .

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,7 @@ function Header ({children, mainHeading}){
 
     if (mainHeading){
         return (<div className='h-10 w-4/5 flex items-center my-1 justify-center text-black' >
-        <div className='h-6 text-2xl my-8 pb-1'>{children}</div>
+        <div className='h-6 text-4xl my-8 pb-2'>{children}</div>
       </div>)
     }
 
@@ -10,7 +10,7 @@ function Header ({children, mainHeading}){
 
 
     return (<div className=' w-full flex items-center my-1 justify-center text-black' >
-    <div className='h-6 pb-1'>{children}</div>
+    <div className='h-6 pb-1 pt-1'>{children}</div>
   </div>
     )
 }
