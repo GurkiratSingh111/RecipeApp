fc92a57e42f4939213a8dd6707f8cba481d095a1
GurkiratSinghArora <sgurkirat868@gmail.com>
2024-03-05 20:20:04 -0800

    Changed layout

--- client/src/components/AddRecipe.js
+++ client/src/components/AddRecipe.js
@@ -58,37 +58,35 @@ function AddRecipe({ allRecipe, setAllRecipe, toast}){
         setIngredients("")
     }
 
-    return  <div className="my-8 pb-5 flex flex-col items-center border-solid border-4 border-yellow-950 rounded-3xl w-4/5 h-full ml-10 mr-10 ">
+    return  <div className="my-4 pb-2 pl-4 flex flex-col items-center rounded-3xl w-4/5" style={{width:"80%"}}>
     <Header mainHeading={true}>Add New Recipe</Header>
     <div className="flex flex-col w-4/5 items-center">
       <Header>Recipe Name</Header>
-      <div className='w-full h-10 text-white'>
-        <input type="text" className='w-full rounded-3xl h-full color-blue pl-5 bg-yellow-950' onChange={nameInputHandler} value={name} arial-label={"Add Recipe Name Here"}/>
+      <div className='w-full h-10 text-black'>
+        <input type="text" className='w-full rounded-3xl h-full color-blue pl-5 border-solid border-black border-2' onChange={nameInputHandler} value={name} arial-label={"Add Recipe Name Here"}/>
       </div>
       <Header>Ingredients</Header>
-      <div className='w-full text-white'>
+      <div className='w-full text-black'>
         <textarea rows="5" // Adjust the number of rows as needed
-        cols="30" type="text"  className='w-full rounded-3xl min-h-30 max-h-50 color-blue pl-8 pt-2 resize-y bg-yellow-950' onChange={ingredientInputHandler} value={ingredients}/>
+        cols="30" type="text"  className='w-full rounded-3xl min-h-30 max-h-50 color-blue pl-8 pt-2 resize-y border-solid border-black border-2' onChange={ingredientInputHandler} value={ingredients}/>
       </div>
       <Header>Directions</Header>
-      <div className='w-full text-white'>
+      <div className='w-full text-black'>
         <textarea rows="5" // Adjust the number of rows as needed
-        cols="30" type="text" className='w-full rounded-3xl min-h-30 max-h-50 color-blue pl-8 pt-2 resize-y bg-yellow-950' onChange={directionInputHandler} value={directions}/>
+        cols="30" type="text" className='w-full rounded-3xl min-h-30 max-h-50 color-blue pl-8 pt-2 resize-y border-solid border-black border-2' onChange={directionInputHandler} value={directions}/>
       </div>
       <div className="flex flex-row justify-center items-center flex-1">
       <button onClick={onResetHandler}
-    className="bg-yellow-950 h-10 
+    className="bg-black h-10 
     w-20 border-solid border-4 
-    border-yellow-950 rounded-3xl m-3
-     hover:bg-yellow-800
-      hover:border-yellow-800"
+    border-black rounded-3xl m-3
+    text-white"
       >Reset</button>
        <button onClick={onSaveHandler}
-    className="bg-yellow-950 h-10 
+    className="bg-black h-10 
     w-20 border-solid border-4 
-    border-yellow-950 rounded-3xl m-3
-     hover:bg-yellow-800
-      hover:border-yellow-800"
+    text-white border-black rounded-3xl m-3
+     hover:bg-red-100-800"
       >Save</button>
       </div>
     </div>
