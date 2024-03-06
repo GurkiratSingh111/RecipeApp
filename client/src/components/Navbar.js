70c9e92a142103c53367527fb089344a574db9bf
GurkiratSinghArora <sgurkirat868@gmail.com>
2024-03-05 20:56:24 -0800

    Added Saved Recipe button

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,9 +2,15 @@ import { MdCookie } from "react-icons/md";
 function Navbar(){
 
     return (
-    <div className='py-3 text-black px-10 text-2xl flex border-black border-b-2'>
-        <span>RecipeStore</span> 
-        <span className="pt-1 pl-1"><MdCookie/></span>
+    <div className='py-3 text-black px-10 text-2xl flex border-black border-b-2 justify-between'>
+        <span className="flex">
+            <span>RecipeStore</span> 
+            <span className="pt-1 pl-1"><MdCookie/></span>
+        </span>
+        <button className="text-lg bg-black text-white rounded-2xl p-2">Saved Recipes</button>
+        
+
+        
 
     </div>);
 
