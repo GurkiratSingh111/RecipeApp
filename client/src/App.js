e3edee32c4a78af5e2f5731da05909e2b1920f67
GurkiratSinghArora <sgurkirat868@gmail.com>
2024-03-05 20:19:41 -0800

    Changed layout

--- client/src/App.js
+++ client/src/App.js
@@ -10,18 +10,30 @@ function App() {
   return (
     <div className="h-full w-full text-white">
       <Navbar />
+      <div className="flex h-auto">
       <div
-        className="flex flex-col items-center min-h-screen min-w-screen px-10 opacity-90"
+        className="flex flex-col items-center px-10 opacity-90"
         style={{
           backgroundImage: `url('bgImage.jpg')`,
-          backgroundPosition: "center",
+          backgroundPosition: "left",
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
+          width: "100%",
+          height: "100%"
         }}
       >
-        <AddRecipe  toast={toast} allRecipe={allRecipe} setAllRecipe={setAllRecipe}/>
-        <AllRecipe allRecipe={allRecipe}/>
+      <div style={{width: "100%", paddingTop:"40px"}} className="flex pl-60 pr-2">
+         <AddRecipe  toast={toast} allRecipe={allRecipe} setAllRecipe={setAllRecipe}/>
+         {/* <AllRecipe allRecipe={allRecipe}/> */}
       </div>
+
+      </div>
+     
+        
+      
+      </div>
+      
+    
       <ToastContainer
           position="top-center"
           autoClose={3000}
