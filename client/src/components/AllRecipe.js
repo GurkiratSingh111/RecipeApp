b9925259a87fcb820e2599b8d44777541668a932
GurkiratSinghArora <sgurkirat868@gmail.com>
2024-03-08 15:18:56 -0800

    Fixing UI

--- client/src/components/AllRecipe.js
+++ client/src/components/AllRecipe.js
@@ -1,18 +1,43 @@
+import { useState, useEffect } from "react";
 import Header from "./Header";
 import RecipeModal from "./RecipeModal";
+import axios from 'axios';
 
-function AllRecipe({allRecipe}){
+function AllRecipe(){
+    const [allRecipe, setAllRecipe] = useState([]);
+    console.log(allRecipe)
+    
+    const fetchData = async () => {
+        try {
+          const response = await axios.get('http://localhost:8081/allRecipes');
+          console.log(response.data)
+          setAllRecipe(response.data);
+        } catch (error) {
+         console.log(error)
+        }
+      };
+    useEffect(() => {
+        fetchData();
+      }, []);
+
+      async function updatedRecipes(){
+        await fetchData();
+      }
    
-    return <div className="my-8 pb-5 flex flex-col items-center border-solid border-4 border-yellow-950 rounded-3xl w-4/5 h-full ml-10 mr-10 ">
-        <Header mainHeading={true}>Find your Saved Recipes Here</Header>
-        <div className=" bg-yellow-950 my-3 py-5 flex flex-col items-center border-solid border-4 border-yellow-950 rounded-3xl w-4/5 h-full ml-10 mr-10 ">
-            {allRecipe && allRecipe.map((recipe,index)=>{
+    return <div className="text-black flex items-center w-full h-full" style={{
+        backgroundImage: `url('bgImage.jpg')`,
+        backgroundPosition: "left",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}>
+        {/* <Header mainHeading={true}>Find your Saved Recipes Here</Header> */}
+        <div className="my-3 py-3 flex flex-col items-center w-4/5 h-full ml-40 pl-9 mr-10" >
+            {allRecipe.length == 0 && <h1 className="text-4xl text-black flex justify-center items-center flex-1">No Recipes Found!!🥺</h1>}
+            {allRecipe.length !== 0 && allRecipe.map((recipe,index)=>{
+                console.log(recipe)
                 return (
-                <div key={index} className="flex flex-col w-4/5 items-left ">
-                    
-                    <RecipeModal recipe={recipe}/>
-
-                    
+                <div key={index} className="flex flex-col w-3/5 items-left">
+                    <RecipeModal recipe={recipe} updatedRecipes={updatedRecipes}/>
                 </div>
                 )
             })
--- server/index.js
+++ server/index.js
@@ -9,36 +9,6 @@ const pool = new Pool({
     password: "root",
 })
 const PORT = 8080;
-
-
-const helper = {
-    init: async function() {
-        const q = "CREATE TABLE IF NOT EXISTS recipes(id SERIAL PRIMARY KEY, name VARCHAR(50), ingredients TEXT, directions TEXT)"
-        await pool.query(q)
-    },
-    add: async function(name, ingredients, directions){
-        const q = "INSERT INTO recipes(name, ingredients, directions) VALUES($1,$2,$3)";
-        await pool.query(q,[name,ingredients,directions]);
-    },
-    delete: async function(id){
-        const q = "DELETE FROM recipes WHERE id = $1";
-        await pool.query(q,[id]);
-    },
-    update: async function(id,name, ingredients, directions){
-        const q = "UPDATE recipes SET name = $1, ingredients = $2, directions = $3 WHERE id = $4";
-        await pool.query(q,[name,ingredients,directions,id]);
-    },
-    allRecipes: async function(){
-        const q = "SELECT * FROM recipes";
-        const result = await pool.query(q);
-        return result.rows;
-    },
-    recipe: async function(id){
-        const q = "SELECT * FROM recipes WHERE id = $1";
-        const result = await pool.query(q,[id]);
-        return result.rows[0];
-    },
-}
 const app = express();
 dotenv.config();
 
@@ -46,9 +16,105 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+function getCurrentTime(date){
+    const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    return `${date.getFullYear()}, ${date.getDate()}th ${month[date.getMonth()]}, ${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`
+}
+
+const helper = {
+    recipeDatabase: async function() {
+        const q = `CREATE TABLE IF NOT EXISTS recipes(
+            id SERIAL PRIMARY KEY, 
+            name VARCHAR(100),
+            instructions TEXT, 
+            timeLastModified VARCHAR(300)
+            )`;
+        await pool.query(q)
+    },
+    ingredientsDatabase: async function() {
+        const q = `CREATE TABLE IF NOT EXISTS ingredients(
+            iid SERIAL PRIMARY KEY, 
+            recipeId INT, name VARCHAR(50), 
+            FOREIGN KEY(recipeId) REFERENCES recipes(id)
+            ON DELETE CASCADE)`
+        await pool.query(q);
+    },
+    add: async function(name, instructions, ingredients){
+        let date = new Date();
+        date = getCurrentTime(date);
+
+        const q = `INSERT INTO 
+                    recipes(name, instructions, timeLastModified) 
+                    VALUES($1, $2, $3) RETURNING *`;
+        const value = await pool.query(q,[name,instructions, date]);
+        const recipeId = value.rows[0].id;
+        const ingredientList = ingredients.split(",").filter( function(e) { return e.trim().length > 0; } );
+        for(let i = 0; i < ingredientList.length; i++){
+            const q = `INSERT INTO ingredients(recipeId, name) VALUES($1,$2)`;
+            await pool.query(q,[recipeId, ingredientList[i]]);
+        }
+    },
+    delete: async function(id){
+        const q = "DELETE FROM recipes WHERE id = $1";
+        await pool.query(q,[id]);
+    },
+    update: async function(id,name, ingredients, instructions){
+        const q1 = `UPDATE recipes 
+                    SET name = $1, 
+                    instructions = $2 
+                    WHERE id = $3`;
+
+        await pool.query(q1,[name,instructions,id]);
+
+        const q2 = `DELETE FROM ingredients
+                    WHERE recipeId = $1`
+        await pool.query(q2,[id]);
+
+        let ingredientList = ingredients.split(",").filter( function(e) { return e.trim().length > 0; } ).map(e=>e.trim())
+        ingredientList = ingredientList.map(e=>e.trim())
+
+        for(let i = 0; i < ingredientList.length; i++){
+            const q3 = `INSERT INTO ingredients(recipeId, name)
+                        VALUES($1,$2)`
+            await pool.query(q3,[id, ingredientList[i]]);
+        }
+       
+    },
+    allRecipes: async function(){
+        // const q = `SELECT r.id, r.name, r.instructions, r.timeLastModified, i.name as ingredientsName
+        //         FROM recipes as r, ingredients as i
+        //         WHERE r.id = i.recipeId`;
+        const q =  `SELECT r.name, r.id
+                    FROM recipes as r`;
+        const result = await pool.query(q);
+        return result.rows;
+    },
+    allIngredients: async function(){
+        const q = "SELECT * FROM ingredients";
+        const result = await pool.query(q);
+        return result.rows;
+    },
+
+    recipeById: async function(id){
+         const q1 = `SELECT *
+                    FROM recipes as r
+                    WHERE r.id = $1`;
+        //const q = "SELECT * FROM recipes WHERE id = $1";
+        let result1 = await pool.query(q1,[id]);
+        result1 =result1.rows[0];
+
+        const q2 = `SELECT i.name
+                    FROM ingredients as i
+                    WHERE i.recipeId = $1`;
+        let result2 = await pool.query(q2,[id]);
+        result2 = result2.rows;
+        return {result1, result2};
+    },
+}
+
 app.post("/add", (req,res)=>{
-    const {name, ingredients, directions} = req.body;
-    helper.add(name, ingredients, directions).then(()=>{
+    const {name, instructions, ingredients} = req.body;
+    helper.add(name, instructions, ingredients).then(()=>{
         res.send("Recipe added");
     }).catch((err)=>{
         res.send("Error adding recipe");
@@ -67,8 +133,8 @@ app.delete("/delete/:id", (req,res)=>{
 
 app.post("/update/:id", (req,res)=>{
     const id = req.params.id;
-    const {name, ingredients, directions} = req.body;
-    helper.update(id,name, ingredients, directions).then(()=>{
+    const {name, ingredients, instructions} = req.body;
+    helper.update(id, name, ingredients, instructions).then(()=>{
         res.send("Recipe updated");
     }).catch((err)=>{
         res.send("Error updating recipe");
@@ -83,9 +149,16 @@ app.get("/allRecipes",(req,res)=>{
     })
 
 })
+app.get("/allIngredients",(req,res)=>{
+    helper.allIngredients().then((data)=>{
+        res.send(data);
+    }).catch((err)=>{
+        res.send("Error fetching ingredients");
+    })
+})
 app.get("/recipe/:id",(req,res)=>{
     const id = req.params.id;
-    helper.recipe(id).then((data)=>{
+    helper.recipeById(id).then((data)=>{
         res.send(data);
     }).catch((err)=>{
         res.send("Error fetching recipe");
@@ -93,12 +166,17 @@ app.get("/recipe/:id",(req,res)=>{
 })
 
 
-helper.init().then(()=>{
+helper.recipeDatabase().then(()=>{
+    helper.ingredientsDatabase().then(()=>{
+        app.listen(PORT,'0.0.0.0');
+        console.log(`Server running on port ${PORT}`);
+    }).catch((err)=>{
+        console.log("Error initializing database");
+    })
     console.log("Database initialized");
 }).catch((err)=>{
     console.log("Error initializing database");
 })
-app.listen(PORT,'0.0.0.0');
-console.log(`Server running on port ${PORT}`);
+
 
 
