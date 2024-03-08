e9e54cd7d96c40f5050477c0e5f7b3c3291d8e39
GurkiratSinghArora <sgurkirat868@gmail.com>
2024-03-08 15:15:04 -0800

    Improved UI

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,7 @@ function Header ({children, mainHeading}){
 
     if (mainHeading){
         return (<div className='h-10 w-4/5 flex items-center my-1 justify-center text-black' >
-        <div className='h-6 text-4xl my-8 pb-2'>{children}</div>
+        <div className='h-6 text-3xl my-8 pb-2'>{children}</div>
       </div>)
     }
 
