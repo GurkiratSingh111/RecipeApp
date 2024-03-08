1836284d4f4370b59583ccd74e092e89c3014aec
GurkiratSinghArora <sgurkirat868@gmail.com>
2024-03-08 15:12:51 -0800

    Added react routers

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import { BrowserRouter } from 'react-router-dom';
+import {Route,Routes} from "react-router-dom";
+import SavedRecipes from './components/SavedRecipes';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/recipes" element={ <SavedRecipes /> } />
+        <Route path="/" element={ <App /> }>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
   </React.StrictMode>
 );
