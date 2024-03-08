a699baeeb19d27380a5c839f698ceaa25b2e060f
GurkiratSinghArora <sgurkirat868@gmail.com>
2024-03-08 15:17:28 -0800

    Added react routers

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,15 @@
 import { MdCookie } from "react-icons/md";
+import { useNavigate } from 'react-router-dom';
 function Navbar(){
+    const navigate = useNavigate();
 
     return (
-    <div className='py-3 text-black px-10 text-2xl flex border-black border-b-2 justify-between'>
+    <div className='py-1 text-black px-10 text-2xl flex border-black border-b-2 justify-between'>
         <span className="flex">
             <span>RecipeStore</span> 
             <span className="pt-1 pl-1"><MdCookie/></span>
         </span>
-        <button className="text-lg bg-black text-white rounded-2xl p-2">Saved Recipes</button>
-        
-
-        
-
+        <button className="text-lg bg-black text-white rounded-2xl p-2" onClick={()=> navigate("/recipes")}>Saved Recipes</button>
     </div>);
 
 }
