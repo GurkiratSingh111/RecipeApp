71c7486bf146d0c07936ba8ce86a6849353e36fb
GurkiratSingh111 <sgurkirat868@gmail.com>
2024-08-27 13:51:22 -0700

    Dummy changes in server folder

--- server/index.js
+++ server/index.js
@@ -5,14 +5,11 @@ const ingredientsDatabase = require( "./models/IngredientModel");
 const  recipeRoutes = require("./routes/recipeRoutes");
 const PORT = 8080;
 const app = express();
-
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
 app.use("/", recipeRoutes)
+
 recipeDatabase().then(()=>{
     ingredientsDatabase().then(()=>{
         app.listen(PORT,'0.0.0.0');
