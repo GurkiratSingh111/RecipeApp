af3463708b1b51c3e5fd07119ccf449f12cebcc1
GurkiratSingh111 <sgurkirat868@gmail.com>
2024-08-28 15:51:06 -0700

    Fixed bug in postgres connection URL

--- server/models/connect.js
+++ server/models/connect.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    connectionString: "postgresql://postgres:root@db:5432/recipe_db"
+    connectionString: "postgresql://postgres:root@postgres-db:5432/recipe_db"
 });
 
 module.exports = pool;
